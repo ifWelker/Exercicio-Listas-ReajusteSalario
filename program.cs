ff14978abe2d1347cf5bd1445a4740f86b474174
ifWelker <84274394+ifWelker@users.noreply.github.com>
2023-02-13 14:55:14 -0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,50 @@
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+           List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ": ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = int.Parse(Console.ReadLine());
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+            Employee emp = list.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
